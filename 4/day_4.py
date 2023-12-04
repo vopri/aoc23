00da0b45dff54377c7651d610ad111002192e7e0
Volker <volker.prior@arvato-scs.com>
2023-12-04 21:43:38 +0100

    Add day 4, part 2

--- 4/day_4.py
+++ 4/day_4.py
@@ -29,12 +29,27 @@ def evaluate_winning_points(your_winning_numbers: list[int]) -> int:
 input_file = "4/test_input.txt"
 input_file = "4/input.txt"
 points: list[int] = []
+matching_no_count: dict[int, int] = dict()  # part 2
+
 with open(input_file) as content:
-    for line in content:
+    for card_id, line in enumerate(content, 1):
         winning_numbers, your_numbers = parse_card(line)
         your_winning_numbers = find_winning_your_winning_numbers(
             winning_numbers, your_numbers
         )
+        matching_no_count[card_id] = len(your_winning_numbers)  # part 2
         points_of_cart = evaluate_winning_points(your_winning_numbers)
         points.append(points_of_cart)
 print("Part 1:", sum(points))
+
+# part 2
+evaluated_cards: dict[int, int] = {}
+for card_id in reversed(matching_no_count.keys()):
+    if matching_no_count[card_id] == 0:
+        evaluated_cards[card_id] = 1
+    else:
+        counter = 1
+        for i in range(card_id + 1, card_id + matching_no_count[card_id] + 1):
+            counter += evaluated_cards[i]
+            evaluated_cards[card_id] = counter
+print("Part 2:", sum(evaluated_cards.values()))
