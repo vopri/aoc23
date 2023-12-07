8b547fc24b4f869ba0e902a85085c3d252cb9d5c
Volker <volker.prior@arvato-scs.com>
2023-12-07 21:28:02 +0100

    Add day 7, part 2

--- 7/day_7.py
+++ 7/day_7.py
@@ -13,8 +13,20 @@ def parse(input_file: str) -> list[Cards]:
         return cardlists
 
 
-def evaluate_type(hand: str) -> int:
+def evaluate_type(hand: str, part_2=False) -> int:
     counter = Counter(hand)
+    if part_2:
+        jokers = counter.get("J", 0)
+        key, amount = counter.most_common()[0]
+        if key == "J":
+            # special case: 'JJJJJ'
+            if hand == "JJJJJ":
+                return 7
+            # special case like 'J6758'
+            key, _ = counter.most_common()[1]
+        counter[key] += jokers
+        counter["J"] -= jokers
+
     match (sorted(counter.values(), reverse=True)):
         # five of a kind
         case [5, *others]:
@@ -34,16 +46,16 @@ def evaluate_type(hand: str) -> int:
         # one pair
         case [2, 1, *others]:
             value = 2
-        case [1, 1, 1, 1, 1]:
-            value = 1
         case _:
-            RuntimeError("Unexpected")
-    return value  # type: ignore
+            value = 1
+    return value
 
 
-def evaluate_value(hand: str) -> list[int]:
+def evaluate_value(hand: str, part_2=False) -> list[int]:
     """If two hands have got the same type"""
     pictures = {"A": 14, "K": 13, "Q": 12, "J": 11, "T": 10}
+    if part_2:
+        pictures["J"] = 1
     card_values = []
     for char in hand:
         if char.isdigit():
@@ -53,15 +65,19 @@ def evaluate_value(hand: str) -> list[int]:
     return card_values
 
 
-def evaluate_strength(hand: str) -> tuple[int, list[int]]:
-    return evaluate_type(hand), evaluate_value(hand)
+def evaluate_strength(hand: str, part_2=False) -> tuple[int, list[int]]:
+    return evaluate_type(hand, part_2), evaluate_value(hand, part_2)
 
 
 input_file = "7/test_input.txt"
 input_file = "7/input.txt"
 
 cardlists = parse(input_file)
-cardlists.sort(key=lambda card: evaluate_strength(card.hand))
 
+cardlists.sort(key=lambda card: evaluate_strength(card.hand))
 total_winnings = sum([(rank * card.bid) for rank, card in enumerate(cardlists, 1)])
 print("Part 1:", total_winnings)
+
+cardlists.sort(key=lambda card: evaluate_strength(card.hand, part_2=True))
+total_winnings = sum([(rank * card.bid) for rank, card in enumerate(cardlists, 1)])
+print("Part 2:", total_winnings)
