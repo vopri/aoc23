fb0756f154ff0e1fd368ae4eba5a1d841ddc5929
Volker <volker.prior@arvato-scs.com>
2023-12-16 18:47:18 +0100

    Add day 11, part 2

--- 11/day_11.py
+++ 11/day_11.py
@@ -3,41 +3,15 @@ from pathlib import Path
 
 
 class Universe:
-    def __init__(self, file_name: str, first_index_1=True):
+    def __init__(self, file_name: str, first_index_1=True, part_1=True):
         self.array = self._parse(file_name)
+        self._factor = 1 if part_1 else 1_000_000 - 1
         self._index_offset = 1 if first_index_1 else 0
-        self._expand()
+        self._empty_rows = self._get_empty_rows_using_offset()
+        self._empty_cols = self._get_empty_cols_using_offset()
 
     def _parse(self, file_name: str) -> list[str]:
         return Path(file_name).read_text().splitlines()
-
-    def _expand(self):
-        self._expand_rows()
-        self._expand_cols()
-
-    def _expand_rows(self):
-        offset = 0
-        local_array = self.array.copy()
-        for row_idx, row in enumerate(local_array):
-            if self._is_empty(row):
-                self.array.insert(row_idx + offset, row)
-                offset += 1
-
-    def _expand_cols(self):
-        empty_cols = self._get_empty_cols()
-        for row_idx, row in enumerate(self.array):
-            offset = 0
-            for col in empty_cols:
-                row = row[: col + offset] + "." + row[col + offset :]
-                self.array[row_idx] = row
-                offset += 1
-
-    def _get_empty_cols(self):
-        return [
-            col_idx
-            for col_idx in range(len(self.array[0]))
-            if self._is_empty(self.get_col(col_idx + self._index_offset))
-        ]
 
     def get(self, col_idx: int, row_idx: int) -> str:
         row = self.array[row_idx - self._index_offset]
@@ -68,13 +42,55 @@ class Universe:
     def get_distance_between_galaxies(
         self, galaxy_1: tuple[int, int], galaxy_2: tuple[int, int]
     ) -> int:
-        return abs(galaxy_1[0] - galaxy_2[0]) + abs(galaxy_1[1] - galaxy_2[1])
+        non_expanded_space = abs(galaxy_1[0] - galaxy_2[0]) + abs(
+            galaxy_1[1] - galaxy_2[1]
+        )
+        return (
+            non_expanded_space
+            + self._get_additional_empty_space_steps_bettween_two_galaxies(
+                galaxy_1, galaxy_2
+            )
+        )
+
+        return non_expanded_space
 
     def _is_empty(self, row_or_col: str) -> bool:
         for symbol in row_or_col:
             if symbol != ".":
                 return False
         return True
+
+    def _get_empty_cols_using_offset(self):
+        return [
+            col_idx
+            for col_idx in range(
+                self._index_offset, len(self.array[0]) + self._index_offset
+            )
+            if self._is_empty(self.get_col(col_idx))
+        ]
+
+    def _get_empty_rows_using_offset(self):
+        return [
+            row_idx
+            for row_idx, row in enumerate(self.array, self._index_offset)
+            if self._is_empty(self.get_row(row_idx))
+        ]
+
+    def _get_additional_empty_space_steps_bettween_two_galaxies(
+        self, g1: tuple[int, int], g2: tuple[int, int]
+    ) -> int:
+        # minus 1, because it's additional
+
+        counter = 0
+        x1, y1 = g1
+        x2, y2 = g2
+        for empty_row in self._empty_rows:
+            if empty_row > min((y1, y2)) and empty_row < max((y1, y2)):
+                counter += 1
+        for empty_col in self._empty_cols:
+            if empty_col > min((x1, x2)) and empty_col < max((x1, x2)):
+                counter += 1
+        return counter * self._factor
 
     def __str__(self):
         return "\n".join([row for row in self.array])
@@ -88,3 +104,12 @@ sum_of_galaxy_distances = sum(
     [universe.get_distance_between_galaxies(g1, g2) for g1, g2 in galaxy_pairs]
 )
 print("Part 1:", sum_of_galaxy_distances)
+
+# part 2
+universe = Universe(file, part_1=False)
+universe._factor
+galaxy_pairs = combinations(universe.find_galaxies(), 2)
+sum_of_galaxy_distances = sum(
+    [universe.get_distance_between_galaxies(g1, g2) for g1, g2 in galaxy_pairs]
+)
+print("Part 2:", sum_of_galaxy_distances)
